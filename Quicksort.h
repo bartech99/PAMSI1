5563efcaf4c7ea2a27de4488bd0aa3f0712f6571
Bartosz Wójcik <bari1005.99@gmail.com>
2020-04-03 20:36:05 +0200

    All done

--- Quicksort.h
+++ Quicksort.h
@@ -14,30 +14,3 @@ void quicksort(int* tab, int start, int end)
         quicksort(tab, pivot + 1, end);
     }
 }
-
-int rev_partition_tab(int* tab, int start, int end) 
-{
-    int pivot = tab[(end + start) / 2];
-    int i = start - 1;
-    int j = end + 1;
-
-    while (1) 
-    {
-        do 
-        {
-            ++i;
-        } while (tab[i] > pivot);
-
-        do 
-        {
-            --j;
-        } while (tab[j] < pivot);
-
-        if (i >= j)
-        {
-            return j;
-        }
-
-        swap_tab(tab, i, j);
-    }
-}
